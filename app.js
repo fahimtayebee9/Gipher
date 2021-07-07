02d4feb1507907489d4b236c518c30e87a8fc8ca
fahimtayebee9 <fahimtayebee9@gmail.com>
2021-07-07 22:19:50 +0600

    Update v2

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const api_key = "kMjDFjbjEFiGcchab2wdsyz1tH3eMYDn";
 const request = new XMLHttpRequest();
 
 const makeRequest = function(url){
+    document.getElementById('grid').innerHTML = "";
     request.open('GET',url);
     request.onload = function(){
         const response = request.response;
         var data = JSON.parse(response);
         for(g in data.data){
             const gif = data.data[g].images.original.url;
+            console.log(gif);
             const col = document.createElement('div');
             col.setAttribute('class', 'col-md-3 mt-3');
             const gifElem = document.createElement('img');
@@ -32,10 +34,10 @@ document.getElementById('search').addEventListener('keyup',function(){
     let search = document.getElementById('search').value;
     if(search != "" || search != null){
         const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&limit=30&q=${search}`;
-        document.getElementById('alert').style.display = 'none';
         makeRequest(url);
     }
     else{
+        document.getElementById('grid').innerHTML = "";
         pageLoad();
     }
 });
